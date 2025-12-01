61dd1b2aa6b008a44f43825adbb4395af4cafbd5
Billy19092024 <billyjoshuaishimwe@gmail.com>
2025-12-01 21:28:18 +0200

    adde new python code and clear readable code

--- quantum.py
+++ quantum.py
@@ -2,36 +2,52 @@ x = 5
 y = "hello infotronics!"
 print(x)
 print(y)
+
 #this is comment.
 #day2 in python
+
 print("this is my first python code but this is beggining of new era")
+
 #python stetments 
 #A computer programing is a list of "instructions" 
 #to be executed by computer  so those instructions are called "statements"
+
 print("python is fun")
 print("hello world!")
 print("one day ill be the senier software engeriner")
 print("learning python gonna be fun trust me !")
+
 #using semicolons optionally
+
 print("hello");print("who will se me in future");print("i promis you u gonna see how fall i come from")
+
 #python output /print
+
 print("hello world!", end=" ")
 print("welcome to python programming")
+
 #python output numbers
+
 print(100)
 print(1000) 
 print(-500)
 print(234*567)
 print(234+234)
+
 #mixing text and number
+
 print("i love this field",100,"murindi","trust me.")
+
 # VARIABLES IN PYTHON
 #A variable is a named location used to store data in the memory.
+
 x = 10
 y ="infotronics"
 print(x)
 print(y)
+
 #changing the value of variable
+
 x = str(3)   
 y = int(3)   
 z = float(3) 
@@ -39,14 +55,18 @@ x = 15
 y = "quantum"
 print(type(x)) 
 print(type(y))
-# real life example what i real so fall 
+
+# real life example what i real so fall
+ 
 name = "joshua"
 print(f"hello {name},your sales today are 50 items!")
 price = 100
 quantity = 5
 total = price+quantity
 print(total)
+
 #my first variable 
+
 myvar = "we believe in this"
 my_var = "and one day we will make it happen"
 _my_var = "never give up"
@@ -59,3 +79,5 @@ print(_my_var)
 print(myVar)
 print(MYVAR)
 print(myvar2)
+
+#
